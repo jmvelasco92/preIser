5b3ad4e0967c7a2b4c69a423d2f440f4b4077923
jmvelasco92 <37741411+jmvelasco92@users.noreply.github.com>
2018-11-22 10:32:06 -0500

    Update settings.py

--- preIser/settings.py
+++ preIser/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$xk=uye&^$o!yr$g9#54t5osd-=*2$txe89k5(z*+c_s28_$v^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,13 +124,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
